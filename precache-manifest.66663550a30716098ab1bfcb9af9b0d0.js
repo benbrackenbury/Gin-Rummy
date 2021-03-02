69a8956de948d55c4e9e81948b475fd31c879803
beb52 <beb52@aber.ac.uk>
2021-03-02 19:27:13 +0000

    Updates

--- precache-manifest.66663550a30716098ab1bfcb9af9b0d0.js
+++ precache-manifest.66663550a30716098ab1bfcb9af9b0d0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "57bee0387c48c7c669866df4b35afa00",
+    "revision": "bb5c57bec0836cb47350e8b500b26759",
     "url": "./index.html"
   },
   {
-    "revision": "1bbffe722487a08bcd2f",
+    "revision": "2c7a901b42465e19bef0",
     "url": "./static/css/main.ba9f4f21.chunk.css"
   },
   {
-    "revision": "fbe13d170ba9a4791aa0",
-    "url": "./static/js/2.05999652.chunk.js"
+    "revision": "ec6572d01fb3174736b3",
+    "url": "./static/js/2.de5990a5.chunk.js"
   },
   {
     "revision": "46f91ec18259a53c6f750cd89192fcc8",
-    "url": "./static/js/2.05999652.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.de5990a5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1bbffe722487a08bcd2f",
-    "url": "./static/js/main.d168b9e1.chunk.js"
+    "revision": "2c7a901b42465e19bef0",
+    "url": "./static/js/main.3622b66e.chunk.js"
   },
   {
     "revision": "88f2c3087498cbaeeae8",
