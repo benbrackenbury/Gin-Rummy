fe9102287e9d455a1cf1c32721c05598249e7b06
beb52 <beb52@aber.ac.uk>
2021-03-02 21:44:50 +0000

    Updates

---
+++ precache-manifest.400530c5fce6f0a58c679dc52196c6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fc8ce45265741f510d6ee26e06d680",
+    "url": "/Gin-Rummy/index.html"
+  },
+  {
+    "revision": "425d556b545493026cd5",
+    "url": "/Gin-Rummy/static/css/main.ba9f4f21.chunk.css"
+  },
+  {
+    "revision": "72c878bf7fd65765c898",
+    "url": "/Gin-Rummy/static/js/2.1f8e2408.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/Gin-Rummy/static/js/2.1f8e2408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425d556b545493026cd5",
+    "url": "/Gin-Rummy/static/js/main.731b0921.chunk.js"
+  },
+  {
+    "revision": "3512128002cce3fa56af",
+    "url": "/Gin-Rummy/static/js/runtime-main.f4d28d34.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/play/precache-manifest.44e6c4dd2c3ef7bc966b81351f899c9f.js"
+  "/Gin-Rummy/precache-manifest.400530c5fce6f0a58c679dc52196c6ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/play/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Gin-Rummy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
