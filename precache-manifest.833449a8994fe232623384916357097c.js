111e74e6c9ef7a590d62381e7cdba41a6229dc66
beb52 <beb52@aber.ac.uk>
2021-03-02 19:21:50 +0000

    Updates

---
+++ precache-manifest.833449a8994fe232623384916357097c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bee0387c48c7c669866df4b35afa00",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1bbffe722487a08bcd2f",
+    "url": "./static/css/main.ba9f4f21.chunk.css"
+  },
+  {
+    "revision": "fbe13d170ba9a4791aa0",
+    "url": "./static/js/2.05999652.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "./static/js/2.05999652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbffe722487a08bcd2f",
+    "url": "./static/js/main.d168b9e1.chunk.js"
+  },
+  {
+    "revision": "88f2c3087498cbaeeae8",
+    "url": "./static/js/runtime-main.c2e69759.js"
+  }
+]);
